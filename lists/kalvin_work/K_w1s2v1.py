f5d0f5a0d662754dad13c6b19f8746a492cf2d69
Kalvin Foster <kfos124@wgu.edu>
2026-04-06 22:03:36 -0700

    Kalvin: Week 1 Session 1 Version 1 & 2 Done!

---
+++ lists/kalvin_work/K_w1s2v1.py
@@ -0,0 +1,248 @@
+# Problem Set Version 1
+
+# -------------------------------------------------------------------------------------------
+# Problem 1: Print List
+# Write a function print_list() that takes in a list lst as a parameter and prints out each item in the list.
+
+# def print_list(lst):
+#     pass
+# Example Input: lst = ["squirtle", "gengar", "charizard", "pikachu"]
+# Example Output:
+
+# squirtle
+# gengar
+# charizard
+# pikachu
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 2: Print Doubled List Items
+# Write a function doubled() that takes in a list of integers lst as a parameter and prints each item in the list multiplied by two.
+
+# def doubled(lst):
+#     pass
+# Example Input: lst = [1,2,3]
+# Example Output:
+
+# 2
+# 4
+# 6
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 3: Return Doubled List
+# Modify the function doubled() so that instead of printing the items, it returns a new list of the doubled numbers.
+
+# Example Usage:
+
+# lst = [1,2,3]
+# new_lst = doubled(lst)
+# print(new_lst)
+# Example Output:
+
+# [2, 4, 6]
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 4: Flip Signs
+# Write a function flip_sign() that takes in a list of integers lst as a parameter and returns a new list where each number in the original list has been multiplied by -1.
+
+# def flip_sign(lst):
+#     pass
+# Example Usage:
+
+# lst = [1,-2,-3,4]
+# flipped_lst = flip_sign(lst)
+# print(flipped_lst)
+# Example Output:
+
+# [-1, 2, 3, -4]
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 5: Max Difference
+# Write a function max_difference() that takes in a list of integers lst and returns the difference between the smallest and largest value in the list.
+
+# def max_difference(lst):
+#     pass
+# Example Usage:
+
+# lst = [5,22,8,10,2]
+# max_diff = max_difference(lst)
+# print(max_diff)
+# Example Output: 20
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 6: Below Threshold
+# Write a function count_less_than() that takes in a list of integers numbers and an integer threshold as parameters and returns the number of items in numbers that are less than threshold.
+
+# def count_less_than(numbers, threshold):
+#     pass
+# Example Usage:
+
+# numbers = [12,8,2,4,4,10]
+# counter = count_less_than(numbers,5)
+# print(counter)
+# Example Output: 3
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 7: Evens List
+# Write a function get_evens() that takes in a list of integers lst as a parameter and returns a list of all even numbers in the list.
+
+# def get_evens(lst):
+#     pass
+# Example Usage:
+
+# lst = [1,2,3,4]
+# evens_lst = get_evens(lst)
+# print(evens_lst)
+# Example Output:
+
+# [2, 4]
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 8: Multiples of Five
+# Write a function multiples_of_five() that prints out multiples of 5 between 1 and 100 (inclusive).
+
+# def multiples_of_five():
+#     pass
+# Example Output:
+
+# 5
+# 10
+# 15
+# 20
+# 25
+# ....
+# 90
+# 95
+# 100
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 9: Divisors
+# Write a function find_divisors() that takes in an integer n as a parameter that returns a list of all divisors of n.
+
+# def find_divisors(n):
+#     pass
+# Example Usage:
+
+# lst = find_divisors(6)
+# print(lst)
+# Example Output:
+
+# [1, 2, 3, 6]
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 10: FizzBuzz
+# Write a function fizzbuzz() that takes in an integer n as a parameter and prints the numbers from 1 to n.
+# For multiples of 3, print "Fizz" instead of the number.
+# For multiples of 5, print "Buzz" instead of the number.
+
+# def fizzbuzz(n):
+#     pass
+# Example Usage: fizzbuzz(13)
+
+# Example Output:
+
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# Fizz
+# 7
+# 8
+# Fizz
+# Buzz
+# 11
+# Fizz
+# 13
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 11: Print the Index
+# Write a function print_indices() that takes in an integer list lst as a parameter and prints out the index of each item in the given list.
+# Use the function range() to loop through the list indices.
+
+# def print_indices(lst):
+#     pass
+# Example Usage:
+
+# lst = [5,1,3,8,2]
+# print_indices(lst)
+# Example Output:
+
+# 0
+# 1
+# 2
+# 3
+# 4
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 12: Linear Search
+# Write a function linear_search() that takes in a list lst and value target as parameters. The function returns the index of target in lst if found. If target is not found in lst, return -1.
+
+# def linear_search(lst, target):
+#     pass
+# Example Usage:
+
+# lst = [1,4,5,2,8]
+# position = linear_search(lst,5)
+# print(position)
+# Example Output: 2
+
+
+# Example Usage:
+
+# lst = [1,4,5,2,8]
+# position = linear_search(lst,10)
+# print(position)
+# Example Output: -1
+
+
+
+
+
+
+
---
+++ lists/kalvin_work/K_w1s2v2.py
@@ -0,0 +1,224 @@
+# Problem Set Version 2
+
+# -------------------------------------------------------------------------------------------
+# Problem 1: Convert Temperature
+# Write a function convertTemp() that takes in celsius as a parameter, which denotes the temperature in celsius. The variable is a non-negative floating point number rounded to two decimal places. In the function, convert celsius into Kelvin and Fahrenheit and return the list ans, in which ans = [kelvin, fahrenheit].
+
+# Note that:
+
+# Kelvin = Celsius + 273.15
+# Fahrenheit = Celsius * 1.80 + 32.00
+
+# def convertTemp(celsius):
+#     pass
+# Example Usage:
+
+# temperatures = convertTemp(23.00)
+# print(temperatures)
+# Example Output:
+
+# [296.15, 73.40]
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 2: Average Score
+# Write a function average() that takes in a list of integers scores as a parameter and returns the average score.
+
+# def average(scores):
+#     pass
+# Example Usage:
+
+# scores = [84,73,92,95,88]
+# avg_score = average(scores)
+# print(avg_score)
+# Example Output: 86.4
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 3: Increment by 1
+# Write a function increment_values() that takes in a list of integers lst as a parameter and returns a new list containing each element incremented by 1.
+
+# def increment_values(lst):
+#     pass
+# Example Usage:
+
+# lst = [3,5,8,2]
+# new_lst = increment_values(lst)
+# print(new_lst)
+# Example Output:
+
+# [4, 6, 9, 3]
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 4: Check for Number
+# Write a function check_num() that takes in a list of integers lst and an integer num as parameters and returns True if num is a value in lst and False otherwise.
+
+# def check_num(lst, num):
+#     pass
+# Example Usage:
+
+# lst = [5,2,3,9,10]
+# flag1 = check_num(lst,9)
+# flag2 = check_num(lst,4)
+# print(flag1)
+# print(flag2)
+# Example Output:
+
+# True
+# False
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 5: Missing Number
+# Write a function find_missing() that takes in a list nums containing n unique numbers in the range [0,n]. The function returns the only number in the range that is missing from the list.
+
+# def find_missing(nums):
+#     pass
+# Example Usage:
+
+# nums = [2,4,1,0,5]
+# missing_num = find_missing(nums)
+# print(missing_num)
+# Example Output: 3
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 6: Reverse List
+# Write a function reverse_list() that takes in a list lst as a parameter and returns a new list containing the elements of lst in reverse order.
+
+# def reverse_list(lst):
+#     pass
+# Example Usage:
+
+# lst = [1,2,3,4,5]
+# rev_lst = reverse_list(lst)
+# print(rev_lst)
+# Example Output:
+
+# [5,4,3,2,1]
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 7: Get Odd Numbers
+# Write a function get_odds() that takes in a list of integers nums and returns a list of all odd numbers in nums.
+
+# def get_odds(nums):
+#     pass
+# Example Usage:
+
+# nums = [2,5,1,8,6,5]
+# odd_nums = get_odds(nums)
+# print(odd_nums)
+# Example Output:
+
+# [5, 1, 5]
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 8: Multiplication Table
+# Write a function multiplication_table() that takes in an integer num and prints the multiples of that integer from 1 to 10.
+
+# def multiplication_table(num):
+#     pass
+# Example Usage: multiplication_table(7)
+
+# Example Output:
+
+# 7
+# 14
+# 21
+# 28
+# 35
+# 42
+# 49
+# 56
+# 63
+# 70
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 9: Create Number
+# Write a function list_to_number() that takes in a list digits where each item is a digit between 0-9. The function returns the number they represent when combined.
+
+# def list_to_number(digits):
+#     pass
+# Example Usage:
+
+# digits = [1,0,3]
+# number = list_to_number(digits)
+# print(number)
+# Example Output: 103
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 10: Move Zeroes
+# Write a function move_zeroes() that takes in an integer list nums and returns a new list with all the 0 values moved to the end of the list. The relative non-zero elements in the original list should be maintained.
+
+# def move_zeroes(nums):
+#     pass
+# Example Usage:
+
+# nums = [1,0,2,3,0,0,4]
+# new_nums = move_zeroes(nums)
+# print(new_nums)
+# Example Output:
+
+# [1,2,3,4,0,0,0]
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 11: Odd Indices
+# Write a function print_odd_indices() that takes in a list nums as a parameter and prints out items at odd indices in the list.
+
+# def print_odd_indices(nums):
+#     pass
+# Example Usage:
+
+# nums = [3,4,8,1,5,2]
+# print_odd_indices(nums)
+# Example Output:
+
+# 4
+# 1
+# 2
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# Problem 12: List Occurrences
+# Write a function find_all_occurrences() that takes in a list lst and a value target as parameters and returns a list of all indices where target is found in lst.
+
+# def find_all_occurrences(lst, target):
+#     pass
+# Example Usage:
+
+# lst = [1,2,6,5,2,1,3,2,2]
+# index_list = find_all_occurrences(lst, 2)
+# print(index_list)
+# Example Output:
+
+# [1,4,7,8]
+
+
