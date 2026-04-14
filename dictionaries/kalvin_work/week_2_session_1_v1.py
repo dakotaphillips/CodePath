93337654b31f23dc159c904aee0f28f9f44b25b9
KalvinFoster <kfos124@wgu.edu>
2026-04-13 19:32:34 -0700

    missing question

--- dictionaries/kalvin_work/week_2_session_1_v1.py
+++ dictionaries/kalvin_work/week_2_session_1_v1.py
@@ -1,19 +1,22 @@
 #Problem Set Version 1
-
+# IGNORE EXTRA NONES IN OUTPUT
 
 # Problem 1: All In
 # Write a function all_in() that takes in a list of integers a and a 
 # list of integers b as parameters. Given these two lists, 
 # return True if every element in list a is in list b. Return False otherwise.
 
-def all_in(a, b):
-    pass
+# def all_in(a, b):
+#     for num in a:
+#         if num not in b:
+#             return False
+#     return True
 
 
-lst_1 = [1, 2]
-lst_2 = [1, 2, 3]
-print(all_in(lst_1, lst_2))
-print(all_in(lst_2, lst_1))
+# lst_1 = [1, 2]
+# lst_2 = [1, 2, 3]
+# print(all_in(lst_1, lst_2))
+# print(all_in(lst_2, lst_1))
 
 # Example Output:
 # True
@@ -31,12 +34,14 @@ print(all_in(lst_2, lst_1))
 # keys[i] should be paired with values[i] in the dictionary where 0 <= i <= len(keys).
 # You may assume keys and values are the same length.
 
-def create_dictionary(keys, values):
-    pass
+# def create_dictionary(keys, values):
+#     return dict(zip(keys, values))
 
 
-keys = ['peanut', 'dragon', 'star', 'pop', 'space']
-values = ['butter', 'fly', 'fish', 'corn', 'ship']
+
+# keys = ['peanut', 'dragon', 'star', 'pop', 'space']
+# values = ['butter', 'fly', 'fish', 'corn', 'ship']
+# print(create_dictionary(keys, values))
 
 # Example Output:
 # {'peanut': 'butter', 'dragon': 'fly', 'star': 'fish', 'pop': 'corn', 'space': 'ship'}
@@ -50,14 +55,17 @@ values = ['butter', 'fly', 'fish', 'corn', 'ship']
 # and it's associated value as "Key: <key>" followed by "Value: <value>". If target
 # is not in dictionary, print "That pair does not exist!".
 
-def print_pair(dictionary, target):
-    pass
+# def print_pair(dictionary, target):
+#     if target not in dictionary:
+#         print("That pair does not exist!")
+#     else:
+#         print(f"Key: {target}")
+#         print(f"Value: {dictionary[target]}")
 
-
-dictionary = {"spongebob": "squarepants", "patrick": "star", "squidward": "tentacles"}
-print_pair(dictionary, "patrick")
-print_pair(dictionary, "plankton")
-print_pair(dictionary, "spongebob")
+# dictionary = {"spongebob": "squarepants", "patrick": "star", "squidward": "tentacles"}
+# print_pair(dictionary, "patrick")
+# print_pair(dictionary, "plankton")
+# print_pair(dictionary, "spongebob")
 
 # Example Output:
 # Key: patrick
@@ -77,17 +85,36 @@ print_pair(dictionary, "spongebob")
 # If the sum of all values is greater than the sum of all keys, the function should return the string "values".
 # If keys and values have equal sums, the function should return the string "balanced".
 
-def keys_v_values(dictionary):
-    pass
+# use atleast 1 loop
+# find sum of all keys
+# find sum of all values
+# if sum of keys > values print keys
+# if sum of values > keys print values
+# else print balanced
+
+# def keys_v_values(dictionary):
+#     key_sum = 0
+#     value_sum = 0
+
+#     for key, value in dictionary.items():
+#         key_sum += key
+#         value_sum += value
+
+#     if key_sum > value_sum:
+#         return "keys"
+#     elif value_sum > key_sum:
+#         return "values"
+#     else:
+#         return "balanced"
 
 
-dictionary1 = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60}
-greater_sum = keys_v_values(dictionary1)
-print(greater_sum)
+# dictionary1 = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60}
+# greater_sum = keys_v_values(dictionary1)
+# print(greater_sum)
 
-dictionary2 = {100:10, 200:20, 300:30, 400:40, 500:50, 600:60}
-greater_sum = keys_v_values(dictionary2)
-print(greater_sum)
+# dictionary2 = {100:10, 200:20, 300:30, 400:40, 500:50, 600:60}
+# greater_sum = keys_v_values(dictionary2)
+# print(greater_sum)
 
 
 # Example Output:
@@ -108,22 +135,28 @@ print(greater_sum)
 # If an item in restock_list is not present in the current_inventory, it 
 # should be added. The function should return the updated dictionary current_inventory.
 
-def restock_inventory(current_inventory, restock_list):
-    pass
+# def restock_inventory(current_inventory, restock_list):
+#     for key in restock_list:
+#         if key in current_inventory:
+#             current_inventory[key] += restock_list[key]
+#         else:
+#             current_inventory[key] = restock_list[key]
+    
+# current_inventory = {
+#     "apples": 30,
+#     "bananas": 15,
+#     "oranges": 10
+# }
 
+# restock_list = {
+#     "oranges": 20,
+#     "apples": 10,
+#     "pears": 5
+# }
 
-current_inventory = {
-    "apples": 30,
-    "bananas": 15,
-    "oranges": 10
-}
-
-restock_list = {
-    "oranges": 20,
-    "apples": 10,
-    "pears": 5
-}
-
+# print(current_inventory)
+# restock_inventory(current_inventory, restock_list)
+# print(current_inventory)
 
 # Example Output:
 # {
@@ -153,12 +186,24 @@ restock_list = {
 
 # A GPA is calculated by finding the average of all grade points.
 
-def calculate_gpa(report_card):
-    pass
-
-
-report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
-print(calculate_gpa(report_card))
+# def calculate_gpa(report_card):
+#     counter = 0
+#     sum = 0
+#     for key, value in report_card.items():
+#         if value == "A":
+#             sum += 4
+#         elif value == "B":
+#             sum += 3
+#         elif value == "C":
+#             sum += 2
+#         elif value == "D":
+#             sum += 1
+#         counter += 1
+    
+#     return sum/counter
+    
+# report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
+# print(calculate_gpa(report_card))
 
 # Example Output: 3.3333333333333335
 
@@ -174,24 +219,32 @@ print(calculate_gpa(report_card))
 # Each dictionary represents data associated with a book, including its title, author, and rating. 
 # The function should return the dictionary for the book with the highest rating.
 
-def highest_rated(books):
-    pass
+# def highest_rated(books):
+#     highest_rated_book = books[0]
+
+#     for book in books:
+#         if book["rating"] > highest_rated_book["rating"]:
+#             highest_rated_book = book
+    
+#     return highest_rated_book
 
 
-books = [
-    {"title": "Tomorrow, and Tomorrow, and Tomorrow",
-     "author": "Gabrielle Zevin",
-     "rating": 4.18
-    },
-    {"title": "A Fortune For Your Disaster",
-     "author": "Hanif Abdurraqib",
-     "rating": 4.47
-    },
-    {"title": "The Seven Husbands of Evenlyn Hugo",
-     "author": "Taylor Jenkins Reid",
-     "rating": 4.40
-    }
-]
+# books = [
+#     {"title": "Tomorrow, and Tomorrow, and Tomorrow",
+#      "author": "Gabrielle Zevin",
+#      "rating": 4.18
+#     },
+#     {"title": "A Fortune For Your Disaster",
+#      "author": "Hanif Abdurraqib",
+#      "rating": 4.47
+#     },
+#     {"title": "The Seven Husbands of Evenlyn Hugo",
+#      "author": "Taylor Jenkins Reid",
+#      "rating": 4.40
+#     }
+# ]
+
+# print(highest_rated(books))
 
 # Expected Output:
 
@@ -207,11 +260,29 @@ books = [
 # Write a function index_to_value_map() that takes in a list lst and returns a 
 # dictionary that maps the index of each element in lst to its value.
 
-def index_to_value_map(lst):
-    pass
+# take a lst
+# map each index to the item in the lst
+# index = key
+# item = value
+# create an empty dict
+# loop through lst
+# take each item/ index using enumerate on the list
+# pass the index as the key 
+# set equal to value
+# then return the dict
+# #
+# def index_to_value_map(lst):
+#     new_dict = {}
+
+#     for index, item in enumerate(lst):
+#         new_dict[index] = item
+    
+#     return new_dict
 
 
-lst = ["apple", "banana", "cherry"]
+# lst = ["apple", "banana", "cherry"]
+
+# print(index_to_value_map(lst))
 
 # Example Output: {0: "apple", 1: "banana", 2: "cherry"}
 
--- dictionaries/kalvin_work/week_2_session_1_v2.py
+++ dictionaries/kalvin_work/week_2_session_1_v2.py
@@ -9,21 +9,28 @@
 # The function should return True if the given list is either monotone increasing or decreasing and False otherwise.
 # Hint: This is a lists problem
 
-def is_monotonic(nums):
-    pass
+# def is_monotonic(nums):
+#     sorted_nums = sorted(nums)
+#     r_sorted_nums = sorted(nums, reverse=True)
+
+#     if nums == sorted_nums or nums == r_sorted_nums:
+#         return True
+#     else:
+#         return False
 
 
-nums1 = [1,2,2,3,10]
-print(is_monotonic(nums1))
 
-nums2 = [12,9,8,3,1]
-print(is_monotonic(nums2))
+# nums1 = [1,2,2,3,10]
+# print(is_monotonic(nums1))
 
-nums3 = [1,1,1]
-print(is_monotonic(nums3))
+# nums2 = [12,9,8,3,1]
+# print(is_monotonic(nums2))
 
-nums4 = [1,9,8,3,5]
-print(is_monotonic(nums4))
+# nums3 = [1,1,1]
+# print(is_monotonic(nums3))
+
+# nums4 = [1,9,8,3,5]
+# print(is_monotonic(nums4))
 
 # Example Output:
 # True
@@ -38,10 +45,16 @@ print(is_monotonic(nums4))
 # as a parameter and returns a dictionary of students, where each student 
 # in student_names is a key mapped to a unique numerical ;' ID.
 
-def student_directory(student_names):
-    pass
-student_names = ["Ada Lovelace", "Tu Youyou", "Mae Jemison", "Rajeshwari Chatterjee", "Alan Turing"]
-
+# def student_directory(student_names):
+#     student_dict = {}
+#     student_id = 1
+#     for student in student_names:
+#          student_dict[student] = student_id
+#          student_id += 1
+#     return student_dict
+         
+# student_names = ["Ada Lovelace", "Tu Youyou", "Mae Jemison", "Rajeshwari Chatterjee", "Alan Turing"]
+# print(student_directory(student_names))
 # Example Output: {"Ada Lovelace": 1, "Tu Youyou": 2, "Mae Jemison": 3, "Rajeshwari Chatterjee": 4, "Alan Turing": 5}
 
 # ----------------------------------------------------------------------------------
@@ -56,14 +69,18 @@ student_names = ["Ada Lovelace", "Tu Youyou", "Mae Jemison", "Rajeshwari Chatter
 # and run the code. Work with your group members to find the cause 
 # of the bug and correctly implement the function.
 
-def get_description(info, keys):
-    for key in keys:
-	    print(info[key])
+# def get_description(info, keys):
+#     for key in keys:
+#         if key in info:
+#             print(info[key])
+#         else:
+#             print("None")
+            
 
 
-info = {"name": "Tom", "age": "30", "occupation": "engineer"}
-keys = ["name", "occupation", "salary"]
-get_description(info, keys)
+# info = {"name": "Tom", "age": "30", "occupation": "engineer"}
+# keys = ["name", "occupation", "salary"]
+# get_description(info, keys)
 
 # Expected Output:
 # Tom
@@ -76,12 +93,22 @@ get_description(info, keys)
 # Write a function sum_even_values() that returns the sum of all 
 # even values in a given dictionary. Assume the dictionary values are all integers.
 
-def sum_even_values(dictionary):
-    pass
+# instantiate a sum var set to 0
+# loop through the dict and retrieve keys, and values
+# check if value is even, use value mod 2 == 0
+# if true, sum += value
+# outside loop return sum_val
+
+# def sum_even_values(dictionary):
+#     sum_val = 0
+#     for key, value in dictionary.items():
+#         if value % 2 == 0:
+#             sum_val += value
+#     return sum_val
 
 
-dictionary = {"a": 4, "b": 1, "c": 2, "d": 8, }
-print(sum_even_values(dictionary))
+# dictionary = {"a": 4, "b": 1, "c": 2, "d": 8, }
+# print(sum_even_values(dictionary))
 
 # Example Output: 14
 
@@ -95,13 +122,17 @@ print(sum_even_values(dictionary))
 # from the second catalog should overwrite the price in the first. 
 # Return the updated first catalog dictionary.
 
-def merge_catalogs(catalog1, catalog2):
-    pass
-
-
-catalog1 = {"apple": 1.0, "banana": 0.5}
-catalog2 = {"banana": 0.75, "cherry": 1.25}
-
+# combine both catalogs
+# if an item in cat2 doesnt exist in cat1, then add it
+# if it does, overwrite the first price with the value if the second one
+# return the first one
+# 
+# forloop over catalog2
+# get key value pair
+# add them to catalog1
+# return catalog1
+#
+#  
 
 # Example Output: {'apple': 1.0, 'banana': 0.75, 'cherry': 1.25}
 
@@ -114,13 +145,19 @@ catalog2 = {"banana": 0.75, "cherry": 1.25}
 # products that have a value less than restock_threshold and need to 
 # be restocked. If products is empty, the function return an empty list.
 
-def get_items_to_restock(products, restock_threshold):
-    pass
+# def get_items_to_restock(products, restock_threshold):
+#     restock = []
+#     for product, quantity in products.items():
+#         if quantity < restock_threshold:
+#             restock.append(product)
+#     return restock
 
 
-products = {"Product1": 10, "Product2": 2, "Product3": 5, "Product4": 3}
-restock_threshold = 5
-
+# products = {"Product1": 10, "Product2": 2, "Product3": 5, "Product4": 3}
+# restock_threshold = 5
+# products2 = {}
+# print(get_items_to_restock(products, restock_threshold))
+# print(get_items_to_restock(products2, restock_threshold))
 # Example Output: ['Product2', 'Product4']
 
 # -----------------------------------------------------------------------------------------
@@ -135,8 +172,24 @@ restock_threshold = 5
 # genre, and rating. The function calculates the average rating for each genre 
 # and returns the genre with the highest average rating.
 
+#  
 def most_popular_genre(movies):
-    pass
+    genre_ratings = {}
+    for movie in movies:
+        if movie["genre"] not in genre_ratings:
+            genre_ratings[movie["genre"]] = [movie["rating"]] 
+        else:
+            genre_ratings[movie["genre"]].append(movie["rating"])
+    
+    large_avg = 0
+    genre = ""
+    for k, v in genre_ratings.items():
+        summed = sum(v)/len(v)
+        if summed > large_avg:
+            large_avg = summed
+            genre = k
+    return genre
+
 
 movies = [
     {"title": "Inception",
@@ -171,12 +224,18 @@ print(most_popular_genre(movies))
 # If the product has a score less than threshold, the function categorizes it as a "fail".
 # The function returns a new dictionary where the key-value pair is the product ID and whether it is a "pass" or "fail".
 
-def quality_control(product_scores, threshold):
-    pass
+# def quality_control(product_scores, threshold):
+#     pass_fail = {}
+#     for product, score in product_scores.items():
+#         if score >= threshold:
+#             pass_fail[product] = "pass"
+#         else: 
+#             pass_fail[product] = "fail"
+#     return pass_fail
 
-product_scores = {"x0123": 75, "x0124": 40, "x0125": 90, "x0126": 55}
-threshold = 60
-print(quality_control(product_scores, threshold))
+# product_scores = {"x0123": 75, "x0124": 40, "x0125": 90, "x0126": 55}
+# threshold = 60
+# print(quality_control(product_scores, threshold))
 
 # Example Output: {'x0123': 'pass', 'x0124': 'fail', 'x0125': 'pass', 'x0126': 'fail'}
 
